2f2c22806fe5f127c49a0b51ca2a78874f11f9bf
Exynos <dwipjana293@gmail.com>
2021-08-22 11:59:28 +0530

    Update Livestream_Video_URL.py

--- Livestream_Video_URL.py
+++ Livestream_Video_URL.py
@@ -14,6 +14,8 @@ async def get_channel_data(url):
                 VideoIDList = re.findall(r"watch\?v=(\S{11})",raw_data) # List of video ids of all livestreams
                 url = f'https://www.youtube.com/watch?v={VideoIDList[0]}' # URL of first YouTube Livestream
                 print(url)
+            else:
+                print('Channel is not currently LIVE')
 
 
 loop = asyncio.get_event_loop()
