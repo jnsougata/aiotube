4f123c40098840d871db07cf594ca29842d093b1
Exynos <dwipjana293@gmail.com>
2021-08-22 11:54:57 +0530

    Update and rename LiveVideoFetcher.py to Livestream_Video_URL.py

--- Livestream_Video_URL.py
+++ Livestream_Video_URL.py
@@ -3,7 +3,7 @@ import aiohttp
 import asyncio
 
 
-URL = 'https://www.youtube.com/channel/CHANNEL_ID' #Replace Channel ID
+URL = 'https://www.youtube.com/channel/CHANNEL_ID' # Replace Channel ID
 
 async def get_channel_data(url):
     async with aiohttp.ClientSession() as session:
@@ -11,8 +11,8 @@ async def get_channel_data(url):
             raw_data = await resp.text()
             data = re.findall(r"\"text\":\" (\S{8})", raw_data)
             if data[0] == 'watching':
-                VideoIDList = re.findall(r"watch\?v=(\S{11})",raw_data)
-                url = f'https://www.youtube.com/watch?v={VideoIDList[0]}'
+                VideoIDList = re.findall(r"watch\?v=(\S{11})",raw_data) # List of video ids of all livestreams
+                url = f'https://www.youtube.com/watch?v={VideoIDList[0]}' # URL of first YouTube Livestream
                 print(url)
 
 
