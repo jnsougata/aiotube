255058511a32cde257b11136b348253e74a1de81
Zen <dwipjana293@gmail.com>
2021-09-29 11:06:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name = 'dya',
-    version = '3.8.5',
+    version = '3.8.6',
     description = 'Get Unlimited YouTube Public Data without YouTubeAPI',
     long_description = readme(),
     long_description_content_type="text/markdown",
