8125b79f1b51911adca5ee132c9988d004a66f26
Zen <jnsougata@gmail.ccom>
2021-11-19 16:31:41 +0530

    optimized

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='aiotube',
-    version='1.3.9',
+    version='1.4.0',
     description='Get YouTube Public Data without YouTubeAPI',
     long_description=readme(),
     long_description_content_type="text/markdown",
