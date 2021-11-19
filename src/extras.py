9322816a791350c9697304808cdb6d3d75dee147
Zen <jnsougata@gmail.ccom>
2021-11-19 14:26:48 +0530

    optimized

--- src/extras.py
+++ src/extras.py
@@ -1,8 +1,7 @@
 import re
-import urllib.request
 from .video import Video
-from .auxiliary import _filter
 from .videobulk import _VideoBulk
+from .auxiliary import _filter, _src
 
 
 class Extras:
@@ -10,76 +9,65 @@ class Extras:
     def __init__(self):
         pass
 
-
     @property
-    def Trending(self):
+    def trending(self):
         """
         :return: < video object > of #1 on trending video
         """
-        url = f'https://www.youtube.com/feed/trending'
-        raw = urllib.request.urlopen(url).read().decode()
+        raw = _src(f'https://www.youtube.com/feed/trending')
         data = re.findall(r"videoId\":\"(.*?)\"", raw)
-        return Video(data[0]) if len(data) != 0 else None
+        return Video(data[0]) if data else None
 
     @property
-    def Music(self):
+    def music(self):
         """
         :return: list of < video object > of trending music videos
         """
-        url = f'https://www.youtube.com/feed/music'
-        raw = urllib.request.urlopen(url).read().decode()
+        raw = _src(f'https://www.youtube.com/feed/music')
         data = re.findall(r"videoId\":\"(.*?)\"", raw)
-        return _VideoBulk(_filter(data)) if len(data) > 0 else None
-
+        return _VideoBulk(_filter(data)) if data else None
 
     @property
-    def Gaming(self):
+    def gaming(self):
         """
         :return: list of < video object > of trending gaming videos
         """
-        url = f'https://www.youtube.com/gaming'
-        raw = urllib.request.urlopen(url).read().decode()
+        raw = _src(f'https://www.youtube.com/gaming')
         data = re.findall(r"videoId\":\"(.*?)\"", raw)
-        return _VideoBulk(_filter(data)) if len(data) > 0 else None
-
+        return _VideoBulk(_filter(data)) if data else None
 
     @property
-    def News(self):
+    def news(self):
         """
         :return: list of < video object > of trending news videos
         """
-        url = f'https://www.youtube.com/news'
-        raw = urllib.request.urlopen(url).read().decode()
+        raw = _src(f'https://www.youtube.com/news')
         data = re.findall(r"videoId\":\"(.*?)\"", raw)
-        return _VideoBulk(_filter(data)) if len(data) > 0 else None
+        return _VideoBulk(_filter(data)) if data else None
 
     @property
-    def Live(self):
+    def livestream(self):
         """
         :return: list of < video object > of trending livestreams
         """
-        url = f'https://www.youtube.com/live'
-        raw = urllib.request.urlopen(url).read().decode()
+        raw = _src(f'https://www.youtube.com/live')
         data = re.findall(r"videoId\":\"(.*?)\"", raw)
-        return _VideoBulk(_filter(data)) if len(data) > 0 else None
+        return _VideoBulk(_filter(data)) if data else None
 
     @property
-    def Learning(self):
+    def learning(self):
         """
         :return: list of < video object > of trending educational videos
         """
-        url = f'https://www.youtube.com/learning'
-        raw = urllib.request.urlopen(url).read().decode()
+        raw = _src(f'https://www.youtube.com/learning')
         data = re.findall(r"videoId\":\"(.*?)\"", raw)
-        return _VideoBulk(_filter(data)) if len(data) > 0 else None
-
+        return _VideoBulk(_filter(data)) if data else None
 
     @property
-    def Sports(self):
+    def sports(self):
         """
         :return: list of < video object > of trending sports videos
         """
-        url = f'https://www.youtube.com/sports'
-        raw = urllib.request.urlopen(url).read().decode()
+        raw = _src(f'https://www.youtube.com/sports')
         data = re.findall(r"videoId\":\"(.*?)\"", raw)
-        return _VideoBulk(_filter(data)) if len(data) > 0 else None
+        return _VideoBulk(_filter(data)) if data else None
