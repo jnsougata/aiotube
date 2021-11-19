5fe94eb45b63ad754102df73fc6fe372e9ce77a9
Zen <jnsougata@gmail.ccom>
2021-11-19 10:27:12 +0530

    optimized

--- src/channelbulk.py
+++ src/channelbulk.py
@@ -1,8 +1,6 @@
 import re
-import urllib.request as req
 from .auxiliary import _src
 from .threads import _Thread
-
 
 
 class _ChannelBulk:
@@ -20,7 +18,7 @@ class _ChannelBulk:
         return [f'{head}{item}' for item in self._ls]
     
     @property
-    def sources(self):
+    def _sources(self):
         head = 'https://www.youtube.com/channel/'
         urls = [f'{head}{item}/about' for item in self._ls]
         def get_page(url):
@@ -29,90 +27,75 @@ class _ChannelBulk:
         
     @property
     def names(self):
-        srcs = self.sources
+        srcs = self._sources
         pattern = r"channelMetadataRenderer\":{\"title\":\"(.*?)\""
         return [re.findall(pattern, item)[0] for item in srcs]
 
     @property
     def subscribers(self):
-        srcs = self.sources
+        srcs = self._sources
         pattern = r"}},\"simpleText\":\"(.*?) "
-        return [re.findall(pattern, item)[0] if re.findall(pattern, item) else None for item in srcs]
+        temp = [re.findall(pattern, item) for item in srcs]
+        return [item[0] if item else None for item in temp]
 
     @property
     def views(self):
-        srcs = self.sources
+        srcs = self._sources
         pattern = r"viewCountText\":{\"simpleText\":\"(.*?)\""
-        return [re.findall(pattern, item)[0][:-6] if re.findall(pattern, item) else None for item in srcs]
+        temp = [re.findall(pattern, item) for item in srcs]
+        return [item[0][:-6] if item else None for item in temp]
 
     @property
     def joined(self):
-        srcs = self.sources
+        srcs = self._sources
         pattern = r"text\":\"Joined \"},{\"text\":\"(.*?)\""
-        return [re.findall(pattern, item)[0] if re.findall(pattern, item) else None for item in srcs]
-
+        temp = [re.findall(pattern, item) for item in srcs]
+        return [item[0] if item else None for item in temp]
+        
     @property
     def countries(self):
-        srcs = self.sources
-        pattern = pattern = r"country\":{\"simpleText\":\"(.*?)\""
-        return [re.findall(pattern, item)[0] if re.findall(pattern, item) else None for item in srcs]
+        srcs = self._sources
+        pattern = r"country\":{\"simpleText\":\"(.*?)\""
+        temp = [re.findall(pattern, item) for item in srcs]
+        return [item[0] if item else None for item in temp]
 
     @property
     def custom_urls(self):
-        def _get_data(url: str):
-            raw = req.urlopen(url).read().decode()
-            pattern = r"\"canonicalChannelUrl\":\"(.*?)\""
-            ins = re.findall(pattern, raw)
-            temp = ins[0] if len(ins) > 0 else None
-            return temp if '/channel' not in temp and temp is not None else None
-
-
-        return _Thread.run(_get_data, [f'https://www.youtube.com/channel/{item}/about' for item in self._ls])
+        srcs = self._sources
+        pattern = r"canonicalChannelUrl\":\"(.*?)\""
+        temp = [re.findall(pattern, item) for item in srcs]
+        return [item[0] if '/channel' not in item[0] else None for item in temp]
 
     @property
-    def description(self):
-        def _get_data(url: str):
-            raw = req.urlopen(url).read().decode()
-            pattern = r"{\"description\":{\"simpleText\":\"(.*?)\"}"
-            temp = re.findall(pattern, raw)
-            return temp[0] if len(temp) > 0 else None
+    def descriptions(self):
+        srcs = self._sources
+        pattern = r"description\":{\"simpleText\":\"(.*?)\""
+        temp = [re.findall(pattern, item) for item in srcs]
+        return [item[0].replace('\\n', ' ') if item else None for item in temp]
 
-        return _Thread.run(_get_data, [f'https://www.youtube.com/channel/{item}/about' for item in self._ls])
 
     @property
-    def avatar_urls(self):
-        def _get_data(url: str):
-            raw = req.urlopen(url).read().decode()
-            pattern = "height\":88},{\"url\":\"(.*?)\""
-            temp = re.findall(pattern, raw)
-            return temp[0] if len(temp) > 0 else None
-
-        return _Thread.run(_get_data, [f'https://www.youtube.com/channel/{item}/about' for item in self._ls])
+    def avatars(self):
+        srcs = self._sources
+        pattern = "height\":88},{\"url\":\"(.*?)\""
+        temp = [re.findall(pattern, item) for item in srcs]
+        return [item[0] if item else None for item in temp]
 
     @property
-    def banner_urls(self):
-        def _get_data(url: str):
-            raw = req.urlopen(url).read().decode()
-            pattern = r"width\":1280,\"height\":351},{\"url\":\"(.*?)\""
-            temp = re.findall(pattern, raw)
-            return temp[0] if len(temp) > 0 else None
-
-        return _Thread.run(_get_data, [f'https://www.youtube.com/channel/{item}/about' for item in self._ls])
+    def banners(self):
+        srcs = self._sources
+        pattern = r"width\":1280,\"height\":351},{\"url\":\"(.*?)\""
+        temp = [re.findall(pattern, item) for item in srcs]
+        return [item[0] if item else None for item in temp]
 
     @property
     def verifieds(self):
-        def _get_data(url: str):
-            raw = req.urlopen(url).read().decode()
-            isVerified = re.search(r'label":"Verified', raw)
-            return True if isVerified else False
-
-        return _Thread.run(_get_data, [f'https://www.youtube.com/channel/{item}/about' for item in self._ls])
+        srcs = self._sources
+        pattern = 'label":"Verified'
+        return [True if re.search(pattern, item) else False for item in srcs]
 
     @property
     def lives(self):
-        def _get_data(url: str):
-            raw = req.urlopen(url).read().decode()
-            isLive = re.search(r'{"text":" watching"}', raw)
-            return True if isLive else False
-
-        return _Thread.run(_get_data, [f'https://www.youtube.com/channel/{item}/about' for item in self._ls])
+        srcs = self._sources
+        pattern = r'{"text":" watching"}'
+        return [True if re.search(pattern, item) else False for item in srcs]
