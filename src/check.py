281ebbf705fc2e8e7b8b3ee162df5b099db68855
Zen <jnsougata@gmail.com>
2022-02-27 03:45:30 +0530

    new features

--- src/check.py
+++
@@ -1,9 +0,0 @@
-import re
-import aiotube
-import locale
-
-# simpleText":"Streamed live
-# [{"gridRenderer":{"items":[{"gridVideoRenderer":{"videoId":"I1pvsCjyQC8"
-
-ch = aiotube.Channel("UCYPvAwZP8pZhSMW8qs7cVCw")
-print(ch.latest.url)
