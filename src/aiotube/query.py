c49927e6cf97615d47f2e7b3d96b89dbd2f0245a
Zen <jnsougata@gmail.com>
2022-03-19 19:53:00 +0530

    enhanced

--- src/aiotube/query.py
+++ src/aiotube/query.py
@@ -15,7 +15,7 @@ class Search:
         pass
 
     @staticmethod
-    def video(keywords: str):
+    def video(keywords: str) -> Video:
         """
         :return: < video object > regarding the query
         """
@@ -23,7 +23,7 @@ class Search:
         return Video(video_ids[0]) if video_ids else None
 
     @staticmethod
-    def channel(keywords: str):
+    def channel(keywords: str) -> Channel:
         """
         :return: < channel object > regarding the query
         """
@@ -31,7 +31,7 @@ class Search:
         return Channel(channel_ids[0]) if channel_ids else None
 
     @staticmethod
-    def playlist(keywords: str):
+    def playlist(keywords: str) -> Playlist:
         """
         :return: < playlist object > regarding the query
         """
@@ -39,34 +39,34 @@ class Search:
         return Playlist(found[0]) if found else None
 
     @staticmethod
-    def videos(keywords: str, limit: int = 20):
+    def videos(keywords: str, limit: int = 20) -> _VideoBulk:
         """
         :param str keywords: query to be searched on YouTube
         :param int limit: total number of videos to be searched
         :return: list of < video object > of each video regarding the query (consider limit)
         """
         video_ids = rgx.video_id.findall(_find_videos(keywords))
-        pure_list = _filter(limit=limit, iterable=raw_ids)
+        pure_list = _filter(limit=limit, iterable=video_ids)
         return _VideoBulk(pure_list) if pure_list else None
 
     @staticmethod
-    def channels(keywords: str, limit: int = 20):
+    def channels(keywords: str, limit: int = 20) -> _ChannelBulk:
         """
         :param str keywords: query to be searched on YouTube
         :param int limit: total number of channels to be searched
         :return: list of < channel object > of each video regarding the query (consider limit)
         """
         channel_ids = rgx.channel_id.findall(_find_channels(keywords))
-        pure_list = _filter(limit=limit, iterable=raw_ids)
+        pure_list = _filter(limit=limit, iterable=channel_ids)
         return _ChannelBulk(pure_list) if pure_list else None
 
     @staticmethod
-    def playlists(keywords: str, limit: int = 20):
+    def playlists(keywords: str, limit: int = 20) -> _PlaylistBulk:
         """
         :param str keywords: query to be searched on YouTube
         :param int limit: total playlists be searched
         :return: list of < playlist object > of each playlist regarding the query (consider limit)
         """
         playlist_ids = rgx.playlist_id.findall(_find_playlists(keywords))
-        pure_list = _filter(limit=limit, iterable=found)
+        pure_list = _filter(limit=limit, iterable=playlist_ids)
         return _PlaylistBulk(pure_list) if pure_list else None
