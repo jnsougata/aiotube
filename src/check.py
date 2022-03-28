097525fa75261b63d79ebd5d9a0ac7f504460d52
Zen <jnsougata@gmail.com>
2022-03-29 00:37:38 +0530

    blah

--- src/check.py
+++
@@ -1,9 +0,0 @@
-from aiotube import Channel, Video, Playlist, Extras, Search
-import time
-
-s = time.perf_counter()
-d = Channel('UCh5HLPIDEDRAWocISKW_JKw')
-print(d.info)
-e = time.perf_counter()
-
-print(f'Channel: {e-s:0.4f}s')
